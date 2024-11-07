4d14a3a638c321e2b48148b70e07ed83ea7cdbc2
jpreyesr <jp.reyesr1@uniandes.edu.co>
2024-11-07 17:25:05 -0500

    cambios sobre apartado list antes de errores

--- src/app/series/series.module.ts
+++ src/app/series/series.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SeriesComponent } from './series.component';
+import { SeriesListComponent } from './series-list/series-list.component';
 
 @NgModule({
   imports: [
